f49d3d4465f8b5ffc0e018989c308cee8739eb69
Timothy Carstens <intoverflow@gmail.com>
2022-11-28 07:20:48 +0000

    Adopt rustbench in miden benchmarks

--- risczero/src/main.rs
+++ risczero/src/main.rs
@@ -1,7 +1,8 @@
 mod benches;
 
+use benches::big_sha2;
 use rustbench::run_jobs;
 
 fn main() {
-    run_jobs(benches::big_sha2::new_jobs());
+    run_jobs::<big_sha2::Job>(big_sha2::new_jobs());
 }
