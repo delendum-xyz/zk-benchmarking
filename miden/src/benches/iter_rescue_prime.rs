f7ab8274ba3cb0bd90a13df2b82f328638720668
Dominik1999 <schmid.dominik.muc@gmail.com>
2022-12-01 19:36:04 +0100

    feat: rescue prime miden hash chain

--- miden/src/benches/iter_rescue_prime.rs
+++ miden/src/benches/iter_rescue_prime.rs
@@ -1,5 +1,5 @@
-use miden::{Assembler, FieldExtension, HashFunction, Program, ProgramInputs, ProofOptions};
-use winter_utils::{group_slice_elements};
+use miden::{Assembler, Program, ProgramInputs, ProofOptions};
+use miden_core::{FieldElement, Felt, ProgramOutputs, StarkField, chiplets};
 use rustbench::Benchmark;
 
 pub struct Job {
@@ -34,26 +34,35 @@ impl Benchmark for Job {
     fn new(num_iter: Self::Spec) -> Self {
         let source = format!(
             "  
+            # stack start: [a3=0, a2=0, a1=0, a0=0, ...]
             begin
+                push.4.0.0.0
+                mem_storew.0 # save [4, 0, 0, 0] into memory location 0
+                swapw
+                push.0.0.0.0 # => stack state [0, 0, 0, 0, a3, a2, a1, a0, 0, 0, 0, 4, ...]
+            
+            # compute hash chain
                 repeat.{}
-                    rphash
+                    rpperm
+                    mem_loadw.0 # overwrite the top word with [4, 0, 0, 0]
+                    swapw.2
+                    mem_loadw.1 # overwrite top word with [0, 0, 0, 0]
                 end
+            
+                # drop everything but the digest from the stack
+                dropw
+                swapw
+                dropw
             end",
             num_iter
         );
 
-        // We need to transform the input_data into 8-byte arrays and 
+        // We can also transform the input_data into 8-byte arrays and
         // then parse each 8-byte array into a u64
-        let mut input_data = [0u8; 32];
-
-        let input = group_slice_elements::<u8, 4>(&input_data)
-            .iter()
-            .map(|&bytes| u32::from_le_bytes(bytes) as u64)
-            .rev()
-            .collect::<Vec<u64>>();
+        let input = vec![0u64; 4];
 
         // Compiling the program
-        let assembler = Assembler::new();
+        let assembler = Assembler::default();
         let program = assembler
             .compile(source.as_str())
             .expect("Could not compile source");
@@ -61,15 +70,7 @@ impl Benchmark for Job {
         let program_input = ProgramInputs::from_stack_inputs(&input).unwrap();
 
         // default (96 bits of security)
-        let proof_options = ProofOptions::new(
-            27,
-            8,
-            16,
-            HashFunction::Blake3_192,
-            FieldExtension::Quadratic,
-            8,
-            256,
-        );
+        let proof_options = ProofOptions::with_96_bit_security();
 
         Job {
             num_iter,
@@ -83,17 +84,71 @@ impl Benchmark for Job {
         &self.num_iter
     }
 
+    /// Compute on VM
     fn guest_compute(&mut self) -> (Self::ComputeOut, Self::ProofType) {
         let program = &self.program;
         let program_input = &self.program_input;
         let proof_options = &self.proof_options;
 
         let (output, proof) = miden::prove(program, program_input, proof_options).expect("results");
-        (Vec::from(output.stack()), proof)
+
+        let stack = output
+            .stack_outputs(4)
+            .iter()
+            .cloned()
+            .rev()
+            .collect::<Vec<_>>();
+
+        (stack, proof)
     }
 
-    fn verify_proof(&self, _output: &Self::ComputeOut, _proof: &Self::ProofType) -> bool {
-        true // TODO!
+    /// Compute on host CPU
+    fn host_compute(&mut self) -> Option<Self::ComputeOut> {
+        // We also hash a vector of four 0's
+        let input = vec![Felt::ZERO; 4];
+        let mut output: [Felt; 4] = input.try_into().unwrap();
+
+        for _ in 0..self.num_iter {
+            output = chiplets::hasher::hash_elements(&output)
+                .as_elements()
+                .try_into()
+                .unwrap();
+        }
+
+        Some(output.iter().map(|x| x.as_int()).collect::<Vec<u64>>())
+    }
+
+    fn verify_proof(&self, output: &Self::ComputeOut, proof: &Self::ProofType) -> bool {
+        let program = &self.program;
+        let stack_inputs = self
+            .program_input
+            .stack_init()
+            .iter()
+            .map(|x| x.as_int())
+            .collect::<Vec<u64>>();
+
+        let mut stack = output
+            .iter()
+            .cloned()
+            .rev()
+            .collect::<Vec<_>>();
+        
+        // We need 16 elements in the stack to verify 
+        let mut stack_rest = vec![0u64; 12];
+        stack.append(&mut stack_rest);
+
+        let program_outputs = ProgramOutputs::new(stack, vec![]);
+
+        let stark_proof = proof.clone();
+
+        let result =
+            miden_verifier::verify(program.hash(), &stack_inputs, &program_outputs, stark_proof)
+                .map_err(|err| format!("Program failed verification! - {}", err));
+
+        match result {
+            Ok(_) => true,
+            Err(_err) => false,
+        }
     }
 
     fn corrupt_proof(&self, proof: Self::ProofType) -> Self::ProofType {
--- miden/src/main.rs
+++ miden/src/main.rs
@@ -1,10 +1,10 @@
 mod benches;
 
-use benches::{iter_blake3, iter_rescue_prime, iter_sha2};
+use benches::iter_rescue_prime;
 use rustbench::run_jobs;
 
 fn main() {
-    //run_jobs::<iter_blake3::Job>(iter_blake3::new_jobs());
-    //run_jobs::<iter_sha2::Job>(iter_sha2::new_jobs());
+    // run_jobs::<iter_blake3::Job>(iter_blake3::new_jobs());
+    // run_jobs::<iter_sha2::Job>(iter_sha2::new_jobs());
     run_jobs::<iter_rescue_prime::Job>(iter_rescue_prime::new_jobs());
 }
